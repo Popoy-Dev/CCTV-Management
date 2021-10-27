fa8fee30a115101365160370e87d65620b7e10be
Kua <popoykua28@gmail.com>
2021-10-27 20:47:21 +0800

    clean code

--- src/pages/Users.js
+++ src/pages/Users.js
@@ -9,29 +9,32 @@ import { Breadcrumb } from "antd";
 export default function Users() {
   const [usersList, IsUserList] = useState(true);
   const handleUsers = () => {
-    console.log("hello");
     IsUserList(true);
   };
   const handleTeams = () => {
-    console.log("hi");
     IsUserList(false);
   };
   return (
     <>
       <div className="bg-light-blue-500 pt-14 pb-28 px-3 md:px-8 h-auto"></div>
-
       <div className="px-3 md:px-8 h-auto -mt-24">
         <div className="container mx-auto max-w-full">
           <div className="grid grid-cols-1 px-4 mb-16">
             <Card>
               <CardHeader color="purple" contentPosition="left">
                 <Breadcrumb>
-                  <Breadcrumb.Item className="text-white text-lg">
-                    <a onClick={handleUsers}> Userss </a>
+                  <Breadcrumb.Item
+                    className="text-white text-lg cursor-pointer"
+                    onClick={handleUsers}
+                  >
+                    Users
                   </Breadcrumb.Item>
 
-                  <Breadcrumb.Item className="text-white text-lg">
-                    <a onClick={handleTeams}> Teams </a>
+                  <Breadcrumb.Item
+                    className="text-white text-lg cursor-pointer"
+                    onClick={handleTeams}
+                  >
+                    Teams
                   </Breadcrumb.Item>
                 </Breadcrumb>
               </CardHeader>
